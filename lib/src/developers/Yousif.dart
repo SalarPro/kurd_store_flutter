48d08e16daf38c01e48dcac8e4516a85044309eb
Yousif-Azad <yousif7plus14@gmail.com>
2023-09-13 18:17:28 +0300

    Add Yousif.dart

---
+++ lib/src/developers/Yousif.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class YousifScreen extends StatefulWidget {
+  const YousifScreen({super.key});
+
+  @override
+  State<YousifScreen> createState() => _YousifScreenState();
+}
+
+class _YousifScreenState extends State<YousifScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return  const Scaffold(backgroundColor: Colors.grey,
+    body: Center(child: Text("mY sCreen")),
+    
+    );
+  }
+}
