c198761b2634d26f9ab71148678c5a6de4fcbb79
Karam-Flutter <karam.flutter@gmail.com>
2023-09-15 03:01:35 +0300

    Add Card Screen

--- lib/src/screens/cart_screen/cart_screen.dart
+++ lib/src/screens/cart_screen/cart_screen.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
+import 'package:kurd_store/src/constants/assets.dart';
+import 'package:kurd_store/src/screens/checkout_screen/checkout_screen.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
@@ -11,8 +15,377 @@ class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Text('Cart Screen'),
-    ));
+      appBar: AppBar(
+        title: Text("Card Screen"),
+      ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Positioned(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Column(
+                    children: [
+                      SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            Column(
+                              children: [
+                                CartCheckOut(),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Divider(
+                                    color: Colors.grey,
+                                    height: 2,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                CartCheckOut(),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Divider(
+                                    color: Colors.grey,
+                                    height: 2,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                CartCheckOut(),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Divider(
+                                    color: Colors.grey,
+                                    height: 2,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                CartCheckOut(),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Divider(
+                                    color: Colors.grey,
+                                    height: 2,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 230,
+                  )
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 50,
+            left: 0,
+            right: 0,
+            child: NavigationBar(),
+          ),
+          Positioned(
+            bottom: 120,
+            left: 0,
+            right: 0,
+            child: rachit(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget CartCheckOut() {
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.all(10),
+              height: 185,
+              width: 170,
+              decoration: BoxDecoration(
+                color: Colors.black12,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Image.asset(
+                Assets.assetsImagesClothCloth2,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Column(
+                // mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'T-Shirt',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    '25,000 IQD',
+                    style: TextStyle(
+                        decoration: TextDecoration.lineThrough,
+                        decorationColor: Colors.red,
+                        decorationThickness: 2,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.red),
+                  ),
+                  Text(
+                    '15,000 IQD',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    '30,000 IQD',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 6),
+                        alignment: Alignment.center,
+                        margin: EdgeInsets.all(5),
+                        // height: 30,
+                        // width: 30,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.amber.withOpacity(0),
+                          border: Border.all(color: Colors.black),
+                        ),
+                        child: Text(
+                          '-',
+                          style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        '2',
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 2),
+                        // height: 30,
+                        // width: 30,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.amber.withOpacity(0),
+                          border: Border.all(color: Colors.black),
+                        ),
+                        child: Text(
+                          '+',
+                          style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: 35,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(5),
+                  height: 35,
+                  width: 35,
+                  decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0),
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.black)),
+                  child: Image.asset(Assets.assetsIconsRemove),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget NavigationBar(
+      // String? iconPath,
+      ) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 90),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => CheckOutScreen()));
+        },
+        child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            height: 60,
+            decoration: BoxDecoration(
+              color: Colors.black.withOpacity(0.8),
+              borderRadius: BorderRadius.circular(25),
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: Container(
+                        padding: EdgeInsets.all(5),
+                        height: 35,
+                        width: 35,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white),
+                        child: Image.asset(Assets.assetsIconsCartConfirm),
+                      ),
+                    ),
+                    Text(
+                      "Check Out",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15),
+                    )
+                  ],
+                ),
+              ],
+            )),
+      ),
+    );
+  }
+
+  Widget rachit(
+      // String? iconPath,
+      ) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        // height: 200,
+        decoration: BoxDecoration(
+          color: Colors.black.withOpacity(0.8),
+          borderRadius: BorderRadius.circular(25),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Products',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                  ),
+                  // SizedBox(
+                  //   width: 200,
+                  // ),
+                  Text(
+                    '100,000 IQD',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Discount',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                  ),
+                  // SizedBox(
+                  //   width: 200,
+                  // ),
+                  Text(
+                    '40,000 IQD',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+              Divider(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Total',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                  ),
+                  // SizedBox(
+                  //   width: 200,
+                  // ),
+                  Text(
+                    '60,000 IQD',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
