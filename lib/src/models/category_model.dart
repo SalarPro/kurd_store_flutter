2f95fe55567aa61631aed0cda00479dce091512d
Karam-Flutter <karam.flutter@gmail.com>
2023-09-19 16:31:50 +0300

    Add Category model and User Model

--- lib/src/models/category_model.dart
+++ lib/src/models/category_model.dart
@@ -1,1 +1,21 @@
 //TODO: crate a model for category to use in firebase
+
+class CategoryModel {
+  final String uid;
+  final String categoryName;
+  final String description;
+
+  CategoryModel({
+    required this.uid,
+    required this.categoryName,
+    required this.description,
+  });
+
+  Map<String, dynamic> categoryModel() {
+    return {
+      'id': uid,
+      'categoryName': categoryName,
+      'email': description,
+    };
+  }
+}
--- lib/src/models/user_model.dart
+++ lib/src/models/user_model.dart
@@ -1,1 +1,27 @@
 //TODO: create user model to use in firebase
+
+class UserModel {
+  final String uid;
+  final String username;
+  final String email;
+  final DateTime createAt;
+  final DateTime updateAt;
+
+  UserModel({
+    required this.uid,
+    required this.username,
+    required this.email,
+    required this.createAt,
+    required this.updateAt,
+  });
+
+  Map<String, dynamic> userModel() {
+    return {
+      'id': uid,
+      'username': username,
+      'email': email,
+      'createAt': createAt,
+      'updateAt': updateAt,
+    };
+  }
+}
