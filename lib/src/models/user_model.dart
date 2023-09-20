b3ddbd3cbd9fcbe177c46c502fc6c96770996312
karam-flutter <karam.flutter@gmail.com>
2023-09-20 16:05:45 +0300

    fix and update

--- lib/src/models/user_model.dart
+++ lib/src/models/user_model.dart
@@ -4,6 +4,8 @@ class UserModel {
   final String uid;
   final String username;
   final String email;
+  final String phone;
+  final String addres;
   final DateTime createAt;
   final DateTime updateAt;
 
@@ -11,6 +13,8 @@ class UserModel {
     required this.uid,
     required this.username,
     required this.email,
+    required this.phone,
+    required this.addres,
     required this.createAt,
     required this.updateAt,
   });
@@ -20,6 +24,8 @@ class UserModel {
       'id': uid,
       'username': username,
       'email': email,
+      'phone': phone,
+      'addres': addres,
       'createAt': createAt,
       'updateAt': updateAt,
     };
